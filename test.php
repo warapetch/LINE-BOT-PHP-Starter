138ca79016ffdd9560ccdf9856202c25864132eb
Warapetch  Ruangpornvisuthi <warapetch.rpvs@gmail.com>
2017-04-18 01:24:39 +0700

    Update test.php

--- test.php
+++ test.php
@@ -13,7 +13,7 @@ curl_setopt( $chOne, CURLOPT_POSTFIELDS, "message=สวัสดี");
 // follow redirects 
 curl_setopt( $chOne, CURLOPT_FOLLOWLOCATION, 1); 
 //ADD header array 
-$headers = array( 'Content-type: application/x-www-form-urlencoded', 'Authorization: Bearer Uad856241c3a152128a0187d4618af584', ); 
+$headers = array( 'Content-type: application/x-www-form-urlencoded', 'Authorization: Bearer E9c+4o7Kfy4N49DvsotR4kI7bZtM6bc8QzZZEcyAarMn0FYEPsIVNVicU7w5BhxcNDelY+ZeMRjk92F8CRniTQXRffGkzhNcP9QVgwUdS9PykBAd1vTSLTfjmL0qmQnucK76cjoDo9e1nX/cbhaxagdB04t89/1O/w1cDnyilFU=', ); 
 curl_setopt($chOne, CURLOPT_HTTPHEADER, $headers); 
 //RETURN 
 curl_setopt( $chOne, CURLOPT_RETURNTRANSFER, 1); 
