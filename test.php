1b7322596f813b6c060fd006006b36e5a1355c27
Warapetch  Ruangpornvisuthi <warapetch.rpvs@gmail.com>
2017-04-18 01:20:10 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,39 +1,26 @@
-<?php
-$headerOptions = array(
- ‘http’=>array(
-   ‘method’=>’POST’,
-   ‘header’=> “Content-Type: application/x-www-form-urlencoded\r\n”
-   .”Authorization: Bearer “.LINE_TOKEN.”\r\n”
-   .”Content-Length: “.strlen($queryData).”\r\n”,
-   ‘content’ => $queryData
- ),
- ‘ssl’=>array(
-   “verify_peer”=>false,
-   “verify_peer_name”=>false,
-  ),
-);
-
-define(‘LINE_API’,”https://notify-api.line.me/api/notify");
- 
-$token = “oDuHH6o6yF3SFkJfRZPitbesB0nEEvwsxdbcwFG0uzw”; //ใส่Token ที่copy เอาไว้
-$str = “Hello”; //ข้อความที่ต้องการส่ง สูงสุด 1000 ตัวอักษร
- 
-$res = notify_message($str,$token);
-print_r($res);
-function notify_message($message,$token){
- $queryData = array(‘message’ => $message);
- $queryData = http_build_query($queryData,’’,’&’);
- $headerOptions = array( 
-         ‘http’=>array(
-            ‘method’=>’POST’,
-            ‘header’=> “Content-Type: application/x-www-form-urlencoded\r\n”
-                      .”Authorization: Bearer “.$token.”\r\n”
-                      .”Content-Length: “.strlen($queryData).”\r\n”,
-            ‘content’ => $queryData
-         ),
- );
- $context = stream_context_create($headerOptions);
- $result = file_get_contents(LINE_API,FALSE,$context);
- $res = json_decode($result);
- return $res;
-}
+<?php 
+$chOne = curl_init(); 
+curl_setopt( $chOne, CURLOPT_URL, "https://notify-api.line.me/api/notify"); 
+// SSL USE 
+curl_setopt( $chOne, CURLOPT_SSL_VERIFYHOST, 0); 
+curl_setopt( $chOne, CURLOPT_SSL_VERIFYPEER, 0); 
+//POST 
+curl_setopt( $chOne, CURLOPT_POST, 1); 
+// Message 
+curl_setopt( $chOne, CURLOPT_POSTFIELDS, "message=สวัสดี"); 
+//ถ้าต้องการใส่รุป ให้ใส่ 2 parameter imageThumbnail และimageFullsize
+//curl_setopt( $chOne, CURLOPT_POSTFIELDS, "message=hi&imageThumbnail=http://www.wisadev.com/wp-content/uploads/2016/08/cropped-wisadevLogo.png&imageFullsize=http://www.wisadev.com/wp-content/uploads/2016/08/cropped-wisadevLogo.png"); 
+// follow redirects 
+curl_setopt( $chOne, CURLOPT_FOLLOWLOCATION, 1); 
+//ADD header array 
+$headers = array( 'Content-type: application/x-www-form-urlencoded', 'Authorization: Bearer Uad856241c3a152128a0187d4618af584', ); 
+curl_setopt($chOne, CURLOPT_HTTPHEADER, $headers); 
+//RETURN 
+curl_setopt( $chOne, CURLOPT_RETURNTRANSFER, 1); 
+$result = curl_exec( $chOne ); 
+//Check error 
+if(curl_error($chOne)) { echo 'error:' . curl_error($chOne); } 
+else { $result_ = json_decode($result, true); 
+echo "status : ".$result_['status']; echo "message : ". $result_['message']; } 
+//Close connect 
+curl_close( $chOne ); ?>
