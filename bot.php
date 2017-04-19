e4dad29d6369cbc684aa09aab1ee8fdee8b96d68
Warapetch  Ruangpornvisuthi <warapetch.rpvs@gmail.com>
2017-04-19 23:16:39 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,5 +1,5 @@
 <?php
-require 'connection.php';  
+//require 'connection.php';  
 
 $access_token = 'E9c+4o7Kfy4N49DvsotR4kI7bZtM6bc8QzZZEcyAarMn0FYEPsIVNVicU7w5BhxcNDelY+ZeMRjk92F8CRniTQXRffGkzhNcP9QVgwUdS9PykBAd1vTSLTfjmL0qmQnucK76cjoDo9e1nX/cbhaxagdB04t89/1O/w1cDnyilFU=';
 
