170f464305ccb95d10ac276b2af5f384fd81f857
Warapetch  Ruangpornvisuthi <warapetch.rpvs@gmail.com>
2017-04-19 22:50:33 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,8 +10,8 @@ $events = json_decode($content, true);
 $datas = json_encode($events);
 
 $userid = $events['events'][0]['source']['userId'];
-$groupid = $events['events'][0]['source']['groupId'];
-$roomid = '';//$events['events'][0]['source']['roomId'];
+//$groupid = '';//$events['events'][0]['source']['groupId'];
+//$roomid = '';//$events['events'][0]['source']['roomId'];
 
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
@@ -30,7 +30,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'ข้อมูลคุณคือ '.$text.' Dynamic Token = '.$replyToken.' Userid = '.$userid.'|Group='+.$groupid+'|Room='.$roomid
+				'text' => 'ข้อมูลคุณคือ '.$text.' Dynamic Token = '.$replyToken.' Userid = '.$userid.'|data='+.$datas
 					];
 
 			// Make a POST Request to Messaging API to reply to sender
