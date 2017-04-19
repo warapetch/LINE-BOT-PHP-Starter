7f89a476a238fd3bf605b279946c9e53ba557bf8
Warapetch  Ruangpornvisuthi <warapetch.rpvs@gmail.com>
2017-04-19 18:22:28 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -24,7 +24,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text.' Token = '.$replyToken.' userid = '.$userid
+				'text' => $text.'/n Token = '.$replyToken.'/n userid = '.$userid
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
