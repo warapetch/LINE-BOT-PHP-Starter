657f208e0ee9e50d9964ea77c0e85402a11e7e26
Warapetch  Ruangpornvisuthi <warapetch.rpvs@gmail.com>
2017-04-20 11:28:55 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,18 +1,21 @@
 <?php
 
-$access_token = 'E9c+4o7Kfy4N49DvsotR4kI7bZtM6bc8QzZZEcyAarMn0FYEPsIVNVicU7w5BhxcNDelY+ZeMRjk92F8CRniTQXRffGkzhNcP9QVgwUdS9PykBAd1vTSLTfjmL0qmQnucK76cjoDo9e1nX/cbhaxagdB04t89/1O/w1cDnyilFU=';
+$channel_access_token = 'E9c+4o7Kfy4N49DvsotR4kI7bZtM6bc8QzZZEcyAarMn0FYEPsIVNVicU7w5BhxcNDelY+ZeMRjk92F8CRniTQXRffGkzhNcP9QVgwUdS9PykBAd1vTSLTfjmL0qmQnucK76cjoDo9e1nX/cbhaxagdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
 
 // Parse JSON
 $events = json_decode($content, true);
-$datas = json_encode($events);
+$raw_text_income  = json_encode($events);
+$is_type_user = false;
 
+// Case Message from ...
 if  ($events['events'][0]['source']['type'] == 'user') 
     {
 		$userid = $events['events'][0]['source']['userId'];
 		$usercaption = ' userid=';
+		$is_type_user = true; // for get user profile
 	}
 else
 
@@ -30,6 +33,7 @@ else
 
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
+	
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		
@@ -41,27 +45,45 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			
+			
+			// Get User Profile
+			if ($is_type_user) {
+				$url = 'https://api.line.me/v2/bot/profile/'.$userid;
+				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $channel_access_token);
+				$ch = curl_init($url);
+				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);				
+				curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+				$result = curl_exec($ch);
+				curl_close($ch);
+				$raw_text_income  = json_encode($result); 
+			}
+			
 			// Build message to reply back
+			//---------------------------------------------------------------------------------------------------------------------
 			if ($text == 'data'){
 			   $messages = [
 				           'type' => 'text',
-							'text' => 'ข้อมูลคุณคือ '.$text.' Dynamic Token = '.$replyToken.' |'.$usercaption.$userid.'| data='.$datas
+							'text' => 'ข้อมูลคุณคือ '.$text.' Dynamic Token = '.$replyToken.' |'.$usercaption.$userid.'| data='.$raw_text_income
 							];
 				}
 			else
 			$messages = [
 				           'type' => 'text',
-							'text' => 'สวัสดี (พิมพ์คำว่า data เพื่อดูข้อมูล) .. Bot Echo = '.$text
+							'text' => 'สวัสดี (พิมพ์คำว่า data เพื่อดูข้อมูล) .. Bot Echo = '.$text.'|'.$raw_text_income
 							];			
 			
+			//---------------------------------------------------------------------------------------------------------------------
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
 			];
+			
 			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $channel_access_token);
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -86,6 +108,7 @@ curl_setopt( $ch, CURLOPT_HEADER, 0);
 curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
 
 $response = curl_exec( $ch );
+curl_close($ch);	
 	
 }
 	
