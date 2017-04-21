88c5a1584c6129d160745500dc16653b151e595c
Warapetch  Ruangpornvisuthi <warapetch.rpvs@gmail.com>
2017-04-21 16:55:21 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -141,12 +141,10 @@ if (!is_null($events['events'])) {
 // save data url
 //$url = 'http://www.plkhealth.go.th/script/updateuser.php';
 $url = 'http://103.253.75.184/updateuser.php';
-	
-$dat_displayname = quoted_printable_decode($dat_displayname);
-$dat_statusmsg   = html_entity_decode(preg_replace("/U\+([0-9A-F]{4,5})/", "&#x\\1;", $dat_statusmsg), ENT_COMPAT | ENT_HTML401, 'UTF-8');	
+//$dat_statusmsg   = html_entity_decode(preg_replace("/U\+([0-9A-F]{4,5})/", "&#x\\1;", $dat_statusmsg), ENT_COMPAT | ENT_HTML401, 'UTF-8');	
 $myvars = 'userid=' . $userid . 
 	  '&display_name='.$dat_displayname.
-	  '&status_message='.$dat_statusmsg. 
+	  '&status_message="'.$dat_statusmsg.'"'. 
 	  '&picture_url='. $dat_pictureurl. 
 	  '&project='.$dat_project_code.
 	  '&group_user='.$dat_project_group_user;
