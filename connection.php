85c6e38672aaf163de04be318c2468d730450f50
Warapetch  Ruangpornvisuthi <warapetch.rpvs@gmail.com>
2017-04-21 15:30:04 +0700

    Delete connection.php

--- connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "203.157.118.72";
-$user = "line";
-$pass = "sa";
-$db="lineapi";
-
-$con = mysql_connect($host,$user,$pass) or die(mysql_error());
-mysql_select_db($db)or die(mysql_error());
-?>
