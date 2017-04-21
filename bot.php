71c64bd9c6d015c2d53fad2a95b392c4b0124ca3
Warapetch  Ruangpornvisuthi <warapetch.rpvs@gmail.com>
2017-04-22 00:14:03 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -17,42 +17,6 @@ function DateThai($strDate)
 return "$strDay $strMonthThai $strYear, $strHour:$strMinute";
 }
 
-function escape_sequence_decode($str) {
-
-    // [U+D800 - U+DBFF][U+DC00 - U+DFFF]|[U+0000 - U+FFFF]
-    $regex = '/\\\u([dD][89abAB][\da-fA-F]{2})\\\u([dD][c-fC-F][\da-fA-F]{2})
-              |\\\u([\da-fA-F]{4})/sx';
-
-    return preg_replace_callback($regex, function($matches) {
-
-        if (isset($matches[3])) {
-            $cp = hexdec($matches[3]);
-        } else {
-            $lead = hexdec($matches[1]);
-            $trail = hexdec($matches[2]);
-
-            // http://unicode.org/faq/utf_bom.html#utf16-4
-            $cp = ($lead << 10) + $trail + 0x10000 - (0xD800 << 10) - 0xDC00;
-        }
-
-        // https://tools.ietf.org/html/rfc3629#section-3
-        // Characters between U+D800 and U+DFFF are not allowed in UTF-8
-        if ($cp > 0xD7FF && 0xE000 > $cp) {
-            $cp = 0xFFFD;
-        }
-
-        // https://github.com/php/php-src/blob/php-5.6.4/ext/standard/html.c#L471
-        // php_utf32_utf8(unsigned char *buf, unsigned k)
-
-        if ($cp < 0x80) {
-            return chr($cp);
-        } else if ($cp < 0xA0) {
-            return chr(0xC0 | $cp >> 6).chr(0x80 | $cp & 0x3F);
-        }
-
-        return html_entity_decode('&#'.$cp.';');
-    }, $str);
-}
 
 $channel_access_token = 'E9c+4o7Kfy4N49DvsotR4kI7bZtM6bc8QzZZEcyAarMn0FYEPsIVNVicU7w5BhxcNDelY+ZeMRjk92F8CRniTQXRffGkzhNcP9QVgwUdS9PykBAd1vTSLTfjmL0qmQnucK76cjoDo9e1nX/cbhaxagdB04t89/1O/w1cDnyilFU=';
 
@@ -181,7 +145,6 @@ $dat_project_group_user	  = 'all';
 			
 		//$dat_displayname = '{"'.$dat_displayname.'"}';
 		//$dat_displayname = json_encode($dat_displayname);
-		$dat_displayname = escape_sequence_decode($dat_displayname);
 			
 		$myvars = 'userid=' . $userid . 
 			  '&display_name='.$dat_displayname.
