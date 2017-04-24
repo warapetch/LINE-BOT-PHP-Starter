39f510ef48a0e7bb04f1aaccb0f5a4c7f8a91776
Warapetch  Ruangpornvisuthi <warapetch.rpvs@gmail.com>
2017-04-24 13:56:56 +0700

    Create setting.php

---
+++ setting.php
@@ -0,0 +1,6 @@
+<?php
+date_default_timezone_set('Asia/bangkok');
+
+define('LINE_NOTIFY_CLIENT_ID', 'TSsCKpdeq6LyZtwzgZjVdF');
+define('LINE_NOTIFY_CLIENT_SECRET', 'Q53ll8T7LXdffYA4WH9yYAgH0WibkF0AHkRXjFCKLph');
+define('CALLBACK_URL', 'https://fitness-thai.herokuapp.com/callback.php');
