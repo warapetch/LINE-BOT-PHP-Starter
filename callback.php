4702dddc2db140686ade08b63d974b5fb04dd2e1
Warapetch  Ruangpornvisuthi <warapetch.rpvs@gmail.com>
2017-04-24 14:02:50 +0700

    Update callback.php

--- callback.php
+++ callback.php
@@ -1,7 +1,7 @@
 <?php
 include __DIR__ . "/settings.php";
 include __DIR__ . "/LineNotifySimpleLib.php";
-$line_notify = new \Uzulla\Net\LineNotifySimpleLib(LINE_NOTIFY_CLIENT_ID, LINE_NOTIFY_CLIENT_SECRET, CALLBACK_URL);
+$line_notify = new LineNotifySimpleLib(LINE_NOTIFY_CLIENT_ID, LINE_NOTIFY_CLIENT_SECRET, CALLBACK_URL);
 $access_token = $line_notify->requestAccessToken($_GET);
 ?>
 <!doctype html>
