f1bec43cf537dd1a8fa26bcafc17269d1de938c7
Warapetch  Ruangpornvisuthi <warapetch.rpvs@gmail.com>
2017-04-26 01:06:43 +0700

    Update callback.php

--- callback.php
+++ callback.php
@@ -1,41 +1,32 @@
 <?php
-// last update : 60-04-22 12.40 //
-
 $client_id = 'TSsCKpdeq6LyZtwzgZjVdF';
 $client_secret = 'Q53ll8T7LXdffYA4WH9yYAgH0WibkF0AHkRXjFCKLph';
-//$session_id = session_id();
-$redirect_uri = 'https://fitness-thai.herokuapp.com/callback.php';
+$redirect_uri = 'http://localhost:8000/show_token.php';
+$code  = $_GET['code'];
+$state = $_GET['state'];
 
-$content = file_get_contents('php://input');
-$data = json_decode($content, true);
-
-echo 'income='.$data."\n\r";
-$code =   $data['code'];
-$state =  $data['state'];
-echo 'code='.$code."\n\r";
-echo 'state='.$state."\n\r";
+$url = 'https://notify-bot.line.me/oauth/token';
+$data = [
+            'grant_type' => 'authorization_code',
+			'code' => $code,
+			'redirect_uri' => $redirect_uri,
+			'client_id' => $client_id ,
+			'client_secret' => $client_secret 
+			   ];
 					
-	/*				$url = 'https://notify-bot.line.me/oauth/token';
-
-					$data = [
-						'grant_type' => ['authorization_code'],
-						'code' => [$code],
-						'redirect_uri' => [$redirect_uri],
-						'client_id' => [$client_id] ,
-						'client_secret' => [$client_secret] 
-					         ];
-					
-					$post = json_encode($data);
-					$headers = 'Content-Type: application/x-www-form-urlencoded';
-					$ch = curl_init($url);
-					curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-					curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-					curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-					curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-					$result = curl_exec($ch);
-					curl_close($ch);
-	
+        $post = json_encode($data);
+		$headers = 'content-type: application/x-www-form-urlencoded';
+		$ch = curl_init($url);
+		curl_setopt($ch, curlopt_customrequest, "post");
+		curl_setopt($ch, curlopt_returntransfer, true);
+		curl_setopt($ch, curlopt_postfields, $post);
+		curl_setopt($ch, curlopt_httpheader, $headers);
+		curl_setopt($ch, curlopt_followlocation, 1);
+		$result = curl_exec($ch);
+		curl_close($ch);
+        
+       
+	/*
 		// save data url
 		$url = 'http://103.253.75.184/post_callback.php';
 				
@@ -51,5 +42,5 @@ echo 'state='.$state."\n\r";
 		curl_close($ch);		
 	*/
 	
-echo 'OK ,content ='.$result.', reponse = '.$response;
+//echo 'OK ,content ='.$result.', reponse = '.$data;
 ?>
