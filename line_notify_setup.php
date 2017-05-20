9e0e107250d0f9976087c4101c54b18ea2d2e4a6
Warapetch  Ruangpornvisuthi <warapetch.rpvs@gmail.com>
2017-05-20 14:19:24 +0700

    Update line_notify_setup.php

--- line_notify_setup.php
+++ line_notify_setup.php
@@ -1,8 +1,13 @@
 <?php
+// file : line_notify_setup.php
+
 date_default_timezone_set('Asia/Bangkok');
 
+define('LINE_NOTIFY_GET_TOKEN_URL','https://notify-bot.line.me/oauth/token');
 define('LINE_NOTIFY_CLIENT_ID', 'TSsCKpdeq6LyZtwzgZjVdF');
 define('LINE_NOTIFY_CLIENT_SECRET', 'Q53ll8T7LXdffYA4WH9yYAgH0WibkF0AHkRXjFCKLph');
-define('CALLBACK_URL', 'https://fitness-thai.herokuapp.com/callback.php');
+
+define('LINE_NOTIFY_CALLBACK_URL', 'http://103.253.75.184/line_notify_callback.php');
+define('POST_LINE_NOTIFY_DATA_URL','http://103.253.75.184/line_notify_post_token.php');
 
 ?>
