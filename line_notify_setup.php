ee813215df7bfc74c389a92c56377ef5a565a0a8
Warapetch  Ruangpornvisuthi <warapetch.rpvs@gmail.com>
2017-05-29 10:16:55 +0700

    Update line_notify_setup.php

--- line_notify_setup.php
+++ line_notify_setup.php
@@ -24,11 +24,23 @@ define('POST_LINE_BOT_DATA_URL','http://103.253.75.184/post_user_id.php');
  
  
  // LINE NOTIFY
- define('LINE_NOTIFY_GET_TOKEN_URL','https://notify-bot.line.me/oauth/token');
-define('LINE_NOTIFY_CLIENT_ID', 'TSsCKpdeq6LyZtwzgZjVdF');
-define('LINE_NOTIFY_CLIENT_SECRET', 'Q53ll8T7LXdffYA4WH9yYAgH0WibkF0AHkRXjFCKLph');
+$Line_Notify_GetToken_URL      = 'https://notify-bot.line.me/oauth/token';
+$Line_Notify_GetAuthorize_URL  = 'https://notify-bot.line.me/oauth/authorize';
+$Line_Notify_Callback_URL  = 'http://103.253.75.184/line_notify_callback.php';
+$Line_Notify_Client_ID     = 'TSsCKpdeq6LyZtwzgZjVdF';
+$Line_Notify_Client_Secret = 'Q53ll8T7LXdffYA4WH9yYAgH0WibkF0AHkRXjFCKLph';
+$Post_Line_Notify_Data_URL = 'http://103.253.75.184/line_notify_post_token.php';
+
+define('LINE_NOTIFY_GET_TOKEN_URL',$Line_Notify_GetToken_URL);
+define('LINE_NOTIFY_CLIENT_ID', $Line_Notify_Client_ID);
+define('LINE_NOTIFY_CLIENT_SECRET', $Line_Notify_Client_Secret);
  
-define('LINE_NOTIFY_CALLBACK_URL', 'http://103.253.75.184/line_notify_callback.php');
-define('POST_LINE_NOTIFY_DATA_URL','http://103.253.75.184/line_notify_post_token.php');
+define('LINE_NOTIFY_CALLBACK_URL', $Line_Notify_Callback_URL);
+define('POST_LINE_NOTIFY_DATA_URL',$Post_Line_Notify_Data_URL);
+
+define('LINE_NOTIFY_AUTHORIZE_URL_FOR_CLIENT',$Line_Notify_GetAuthorize_URL.'?response_type=code'.
+              '&client_id='+$Line_Notify_Client_ID.'&redirect_uri='+$Line_Notify_Callback_URL+
+              '&scope=notify'+
+              '&state='; // State wait for Bot
 
 ?>
