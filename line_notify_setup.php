482e8915bba0131ff1431b6f244bcc33ef1a1dcc
Warapetch  Ruangpornvisuthi <warapetch.rpvs@gmail.com>
2017-05-29 10:22:36 +0700

    Update line_notify_setup.php

--- line_notify_setup.php
+++ line_notify_setup.php
@@ -24,22 +24,22 @@ define('POST_LINE_BOT_DATA_URL','http://103.253.75.184/post_user_id.php');
  
  
  // LINE NOTIFY
-$Line_Notify_GetToken_URL      = 'https://notify-bot.line.me/oauth/token';
-$Line_Notify_GetAuthorize_URL  = 'https://notify-bot.line.me/oauth/authorize';
-$Line_Notify_Callback_URL  = 'http://103.253.75.184/line_notify_callback.php';
-$Line_Notify_Client_ID     = 'TSsCKpdeq6LyZtwzgZjVdF';
-$Line_Notify_Client_Secret = 'Q53ll8T7LXdffYA4WH9yYAgH0WibkF0AHkRXjFCKLph';
-$Post_Line_Notify_Data_URL = 'http://103.253.75.184/line_notify_post_token.php';
+$XLine_Notify_GetToken_URL      = 'https://notify-bot.line.me/oauth/token';
+$XLine_Notify_GetAuthorize_URL  = 'https://notify-bot.line.me/oauth/authorize';
+$XLine_Notify_Callback_URL  = 'http://103.253.75.184/line_notify_callback.php';
+$XLine_Notify_Client_ID     = 'TSsCKpdeq6LyZtwzgZjVdF';
+$XLine_Notify_Client_Secret = 'Q53ll8T7LXdffYA4WH9yYAgH0WibkF0AHkRXjFCKLph';
+$XPost_Line_Notify_Data_URL = 'http://103.253.75.184/line_notify_post_token.php';
 
-define('LINE_NOTIFY_GET_TOKEN_URL',$Line_Notify_GetToken_URL);
-define('LINE_NOTIFY_CLIENT_ID', $Line_Notify_Client_ID);
-define('LINE_NOTIFY_CLIENT_SECRET', $Line_Notify_Client_Secret);
+define('LINE_NOTIFY_GET_TOKEN_URL',$XLine_Notify_GetToken_URL);
+define('LINE_NOTIFY_CLIENT_ID', $XLine_Notify_Client_ID);
+define('LINE_NOTIFY_CLIENT_SECRET', $XLine_Notify_Client_Secret);
  
-define('LINE_NOTIFY_CALLBACK_URL', $Line_Notify_Callback_URL);
-define('POST_LINE_NOTIFY_DATA_URL',$Post_Line_Notify_Data_URL);
+define('LINE_NOTIFY_CALLBACK_URL', $XLine_Notify_Callback_URL);
+define('POST_LINE_NOTIFY_DATA_URL',$XPost_Line_Notify_Data_URL);
 
-define('LINE_NOTIFY_AUTHORIZE_URL_FOR_CLIENT',$Line_Notify_GetAuthorize_URL.'?response_type=code'.
-              '&client_id='.$Line_Notify_Client_ID.'&redirect_uri='.$Line_Notify_Callback_URL.
+define('LINE_NOTIFY_AUTHORIZE_URL_FOR_CLIENT',$XLine_Notify_GetAuthorize_URL.'?response_type=code'.
+              '&client_id='.$XLine_Notify_Client_ID.'&redirect_uri='.$XLine_Notify_Callback_URL.
               '&scope=notify&state='; // ค่า State รอ BOT เอา DisplayName มาผนวก
 
 ?>
